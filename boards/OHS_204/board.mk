7d57c4aca6d90970aec1a3b7a60d7319aea35162
Rui Barreiros <rbarreiros@gmail.com>
2020-07-22 01:31:23 +0000

    Initial code changes

---
+++ boards/OHS_204/board.mk
@@ -0,0 +1,9 @@
+# List of all the board related files.
+BOARDSRC = $(BOARDS)/OHS_204/board.c
+
+# Required include directories
+BOARDINC = $(BOARDS)/OHS_204
+
+# Shared variables
+ALLCSRC += $(BOARDSRC)
+ALLINC  += $(BOARDINC)
---
+++ boards/OHS_F767/board.mk
@@ -0,0 +1,9 @@
+# List of all the board related files.
+BOARDSRC = $(BOARDS)/OHS_F767/board.c
+
+# Required include directories
+BOARDINC = $(BOARDS)/OHS_F767
+
+# Shared variables
+ALLCSRC += $(BOARDSRC)
+ALLINC  += $(BOARDINC)
